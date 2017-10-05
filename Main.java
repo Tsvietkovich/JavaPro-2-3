8d1c48b4d56fd56e476778a9775335fe2feb377c
Mariya Tsvietkovich <tsvetkovich@gmail.com>
2017-10-05 09:27:08 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         String urlStr = "http://query.yahooapis.com/v1/public/yql?format=xml&q=select%20*%20from%20" +
                 "yahoo.finance.xchange%20where%20pair%20in%20(\"USDEUR\",%20\"USDUAH\")&env=store://datatables.org/alltableswithkeys";
-        String path = "E:\\JAVA\\Projects\\OOP\\src\\pro\\lecture_2\\Yahoo\\file.xml";
+        String path = "file.xml";
         try {
            String xmlString  = new ReaderURL().readURLToString(urlStr);
            new ConverterXML().createXMLDoc(xmlString,path);
